c2a9399df7dcc305954aabc6d914704fc5d58251
william chou <w.chou06@gmail.com>
2017-03-01 12:04:13 -0500

    Setup skeleton, verified running, Update instructions

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 module.exports = {
 
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: process.env.DB_SSL
     },
     migrations: {
       directory: './db/migrations',
