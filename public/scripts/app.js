01e09df306a54ecda2b2d05d23bca7fa7d2bce8a
william chou <w.chou06@gmail.com>
2017-03-02 13:27:15 -0500

    Started food list skeleton.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,14 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+  $("#grocery-list").on("click", "input:last-of-type", (ev) => {
+    console.log(ev.target);
+
+  });
 });
