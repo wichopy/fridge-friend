42667d6d1b3409f651b4c859ae6c8744f9b34323
william chou <w.chou06@gmail.com>
2017-03-01 20:21:10 -0500

    Create routes for user authentication.

---
+++ routes/session.js
@@ -0,0 +1,45 @@
+"use strict";
+//http://stackoverflow.com/questions/7140074/restfully-design-login-or-register-resources
+const express = require('express');
+const router = express.Router();
+
+module.exports = (knex) => {
+
+  //Grab all the users
+  // router.get("/", (req, res) => {
+  //   knex
+  //     .select("*")
+  //     .from("users")
+  //     .then((results) => {
+  //       res.json(results);
+  //     });
+  // });
+
+  //login
+  router.post("/", (req, res) => {
+    knex
+      .select("*")
+      .from("users")
+      .where({ name: req.body.name })
+      .then((results) => {
+        console.log(results);
+        res.status(200).send();
+      });
+  });
+
+  //logout
+  router.delete("/", (req, res) => {
+    // console.log(req.body.id);
+    knex
+      .select("*")
+      .from("users")
+      .where({ id: req.body.id }) //query WHERE ID = req.body.id
+      .then((results) => {
+        console.log(`Your user is: ${results[0].name}`);
+        res.status(200).send();
+      });
+  });
+
+
+  return router;
+}
