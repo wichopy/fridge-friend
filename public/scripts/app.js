764a3c019a1dc29722c1b2e43125ccd49b150b96
Tory Shoreman <toryshoreman@gmail.com>
2017-03-03 15:52:18 +0000

    Adding working lists

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,34 @@
 $(document).ready( () => {
-  console.log($(".login"));
   $(".login").hide()
   $("#already").on("click", function(){
     $("#already").hide();
     $(".register").hide();
     $(".login").show(400);
 });
-});
-
-
-
-
-$(() => {
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });;
-  $("#grocery-list").on("click", "input:last-of-type", (ev) => {
-    console.log(ev.target);
-
+//// add a new list well
+  $("#grocery-list").on("focus", ".food-item", function() {
+    $("#grocery-list").append(`<div class="input-group">
+                                 <span class="input-group-addon">
+                                   <input type="checkbox">
+                                  </span>
+                                  <input type="text" class="form-control food-item" name="food-item">
+                               </div>`);
   });
 });
+
+
+// $(() => {
+//   // $.ajax({
+//   //   method: "GET",
+//   //   url: "/api/users"
+//   // }).done((users) => {
+//   //   for(user of users) {
+//   //     $("<div>").text(user.name).appendTo($("body"));
+//   //   }
+//   // });;
+//   $("#grocery-list").on("click", "input:last-of-type", (ev) => {
+//     console.log(ev.target);
+
+//     });
+//   });
+
