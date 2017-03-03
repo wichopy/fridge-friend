08db88a674a384020fde8807fb3638c7877e9d6f
william chou <w.chou06@gmail.com>
2017-03-03 13:17:18 -0500

    skeleton layout for database interaction.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(() => {
 
   $(".login").hide()
@@ -7,20 +6,20 @@ $(document).ready(() => {
     $(".register").hide();
     $(".login").show(400);
 
-});
-//// add a new list well
-  $("#grocery-list").on("focus", ".food-item", function() {
+  });
+  //// add a new list well
+  $("#grocery-list").on("focus", ".food-item", function () {
     $("#grocery-list").append(`<div class="input-group">
                                  <span class="input-group-addon">
                                    <input type="checkbox">
                                   </span>
                                   <input type="text" class="form-control food-item" name="food-item">
+                                  <input type="number" class="form-control food-qty" name="food-qty" placeholder="qty">
                                </div>`);
+    var fakeQtyData = [3, 4, 5, 6, 7];
 
-
-
+    // $(".inv-qty").
 
 
   });
 });
-
