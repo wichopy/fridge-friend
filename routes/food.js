b2c5969a3f193c98cf417ccb4c3f5667724c0b04
Benton <chan.benton+test@gmail.com>
2017-03-04 15:31:30 +0000

    food.js needs a fixin', incomplete code provided

--- routes/food.js
+++ routes/food.js
@@ -29,28 +29,105 @@ module.exports = (knex) => {
   });
 
   food.post("/shopping", (req, res) => {
-    console.log("inside post: shopping");
-    console.log(req.body);
-    res.status(200).send();
+   /* 1. WARNING: Single entries from front-end result in a string element, multiple entries result in an array element for key:foodItem
+    * 2. If logged in:
+    * 3. Loop through each item doing the following:
+    * 4. Goes through the below only if there is a food-item/food-qty:
+    * 5. var function appendIng: appends an item to list "ingredients" if it does not exist (via knex query)
+    * 6. var function appendInv: obtains current item's count to itemTotal
+    * 6a. ELSE, creates a new entry, adds userId(userId), ingId(ingIndex), pend(food-qty), and inventory (0).
+    * 7. promise.all (appendIng,appendInv)
+    * 8. Does not complete as intended, as insert commands don't succeed in either row [/tableflip]
+    * 9. Upgrade permission issue??
+    */
 
-    // if (req.session.user_id) {
-    //   knex
-    //     .select("*")
-    //     .from("inventory")
-    //     .where({ userId: req.session.user_id })
-    //     .then((results) => {
+    let userId = req.session.user_id;
+    let foodItem = req.body["food-item"];
+    let foodQty = req.body["food-qty"];
+    if (userId) {
+      for (let index in foodItem){
+        let curFood = foodItem[index];
+        if (foodItem && foodQty){
+          var appendIng = new Promise(() => {
+            knex.select("*")
+              .from("ingredients")
+              .where({name: curFood})
+              .then((results) => {
+                if (!results[0]){ // checks if there is no results from the search, then it appends to ing
+                  knex.insert({name: curFood}).into("ingredients");
+                }  
+              });
+          });
+          
+          var appendInv = new Promise((resolve,reject) => {
+            knex.select("userId")
+            .from("inventory")
+            .where({ingId: ingIndex})
+            .then ((results) => {            
+              if (let userId in results){
+                let invTotal = 0;
+                knex.select("pend")
+                  .from("inventory")
+                  .where({
+                    ingId: ingIndex,
+                    userId: userId
+                  })
+                  .then ((results) => {
+                    invTotal = results[0] + foodQty[index];
+                  })
 
-    //     });
-    // } else {
-    //   res.redirect("/");
-    // }
+                knex('inventory')
+                  .where({
+                    id: userId
+                  }) 
+                  .update({
+                    pend: invTotal
+                  });
 
+              } else {
+                knex("ingredients").insert({
+                  userId: userId,
+                  ingId: ingIndex,
+                  pend: foodQty[index],
+                  qty: 0
+                }); // knex insert
+              } // else statement
+            }); // .then
+          }); // Promise, end.
+        } // if item+qty is food, ends.
+        Promise.all([appendIng, appendInv]).then().catch();
+      } // for loop cycling each submitted item
+    } else {
+      res.redirect("/");
+    }
   });
 
   // Purchase button is clicked: removes from shopping list and appends under inventory list
   food.post("/inventory", (req, res) => {
     if (req.session.user_id) {
       console.log(req.body);
+      let foodItem = req.body["food-item"];
+      for (let index in foodItem){
+        var pendCount = 0;
+        knex
+          .select("pend")
+          .from ("inventory")
+          .innerJoin("ingredients", 'inventory.ingId', 'ingredients.id')
+          .where({name: curFood})
+          .then((results) =>{
+            pendCount = results[0];
+          });
+
+        knex
+          .select("*")
+          .from ("inventory")
+          .innerJoin("ingredients", 'inventory.ingId', 'ingredients.id')
+          .where({name: curFood})
+          .update({
+            pend: 0,
+            total: pendCount
+          });
+      }
       res.status(200).send()
     } else {
       res.redirect("/");
