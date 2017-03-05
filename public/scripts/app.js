6f1bcd03debb396c309fbc75bc49f584806cce87
Tory Shoreman <toryshoreman@gmail.com>
2017-03-05 00:34:28 +0000

    Adding styled list boxes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -55,7 +55,7 @@ $(document).ready(() => {
       ev.preventDefault();
       var checkedItems = [];
       console.log($(".checked-list-box li.active"))
-      $(".checked-list-box li.active").each(function(idx, li) {
+      $(".checked-list-box.shopping li.active").each(function(idx, li) {
          $.ajax({
         url: "/food/inventory",
         method: 'POST',
@@ -69,7 +69,7 @@ $(document).ready(() => {
     }
   });
   // $(function () {
-    $('.list-group.checked-list-box .list-group-item').each(function () {
+    $('.list-group.checked-list-box.shopping .list-group-item').each(function () {
 
         // Settings
         var $widget = $(this),
@@ -145,6 +145,82 @@ $(document).ready(() => {
         });
         $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
     });
-// });
+///////////// inventory
+ $('.list-group.checked-list-box.inventory .list-group-item').each(function () {
+         console.log(this);
+        // Settings
+        var $widget = $(this),
+            $checkbox = $('<input type="checkbox" class="hidden" />'),
+            color = ($widget.data('color') ? $widget.data('color') : "primary"),
+            style = ($widget.data('style') == "button" ? "btn-" : "inventory-list-group-item-"),
+            settings = {
+                on: {
+                    icon: 'glyphicon glyphicon-check'
+                },
+                off: {
+                    icon: 'glyphicon glyphicon-unchecked'
+                }
+            };
+
+        $widget.css('cursor', 'pointer')
+        $widget.append($checkbox);
+
+        // Event Handlers
+        $widget.on('click', function () {
+            $checkbox.prop('checked', !$checkbox.is(':checked'));
+            $checkbox.triggerHandler('change');
+            updateDisplay();
+        });
+        $checkbox.on('change', function () {
+            updateDisplay();
+        });
+
+
+        // Actions
+        function updateDisplay() {
+            var isChecked = $checkbox.is(':checked');
+
+            // Set the button's state
+            $widget.data('state', (isChecked) ? "on" : "off");
+
+            // Set the button's icon
+            $widget.find('.state-icon')
+                .removeClass()
+                .addClass('state-icon ' + settings[$widget.data('state')].icon);
+
+            // Update the button's color
+            if (isChecked) {
+                $widget.addClass(style + color + ' active');
+            } else {
+                $widget.removeClass(style + color + ' active');
+            }
+        }
+
+        // Initialization
+        function init() {
+
+            if ($widget.data('checked') == true) {
+                $checkbox.prop('checked', !$checkbox.is(':checked'));
+            }
+
+            updateDisplay();
+
+            // Inject the icon if applicable
+            if ($widget.find('.state-icon').length == 0) {
+                $widget.prepend('<span class="state-icon ' + settings[$widget.data('state')].icon + '"></span>');
+            }
+        }
+        init();
+    });
+
+    $('#get-checked-data').on('click', function(event) {
+        event.preventDefault();
+        var checkedItems = {}, counter = 0;
+        $("#check-list-box li.active").each(function(idx, li) {
+            checkedItems[counter] = $(li).text();
+            counter++;
+        });
+        $('#display-json').html(JSON.stringify(checkedItems, null, '\t'));
+    });
 
 });
